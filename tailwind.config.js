e8f7b038c6e5aea8fd8129df47b45b5128fde182
wai129yan <waiyan.msc.yu12@gmail.com>
2025-03-05 19:01:14 +0900

    cv/creating

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,16 @@ module.exports = {
 
        
          "text-color": "#e5e7eb"
-
-    
       },
+      Shadow: {
+        'light': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        'medium': '0 10px 15px -3px rgba(0, 0, 0, 0.2), 0 4px 6px -2px rgba(0, 0, 0, 0.1)',
+        'dark': '0 20px 25px -5px rgba(0, 0, 0, 0.3), 0 10px 10px -5px rgba(0, 0, 0, 0.2)',
+        'inner-light': 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        'inner-dark': 'inset 0 4px 6px -1px rgba(0, 0, 0, 0.2)',
+        'colored-blue': '0 10px 15px -3px rgba(59, 130, 246, 0.3), 0 4px 6px -2px rgba(59, 130, 246, 0.1)',
+        'colored-green': '0 10px 15px -3px rgba(34, 197, 94, 0.3), 0 4px 6px -2px rgba(34, 197, 94, 0.1)'
+      }
     },
   },
   plugins: [],
